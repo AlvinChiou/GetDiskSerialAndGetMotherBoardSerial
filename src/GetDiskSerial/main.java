ea316176e9b78ebb697aaec6303de6fa7742a404
AlvinChiou <alvinhsian@gmail.com>
2014-03-27 14:03:31 +0800

    Initial commit

---
+++ src/GetDiskSerial/main.java
@@ -0,0 +1,8 @@
+package GetDiskSerial;
+
+public class main {
+    public static void main(String[] args) {
+        String DiskSerial = GetDiskSerial.getSerialNumber("C");
+        System.out.println("DiskSerial:"+DiskSerial);
+    }
+}
---
+++ src/GetMotherBoardSerial/main.java
@@ -0,0 +1,8 @@
+package GetMotherBoardSerial;
+
+public class main {
+    public static void main(String[] args) {
+        String MotherBoardSN = GetMotherBoardSerial.getMotherboardSN();
+        System.out.println("MotherBoardSN:"+MotherBoardSN);
+    }
+}
